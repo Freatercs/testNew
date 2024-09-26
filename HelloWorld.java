89014bf31159c7b26e04962f21741f51db36e1db
Freatercs <mrmade6@gmail.com>
2024-09-26 10:02:38 +0300

    Добавили перенос строки после new feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String [] args){
         System.out.println("Hello World\n");
-        System.out.println("New Feature");
+        System.out.println("New Feature\n");
     }
 }
 
